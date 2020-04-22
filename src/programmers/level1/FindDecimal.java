8269647e1b9b90294042d80ae018a4f568569012
youngsun-choi <youngsunchoi42@gmail.com>
2020-04-22 17:11:23 +0900

    Add findDecimal

---
+++ src/programmers/level1/FindDecimal.java
@@ -0,0 +1,52 @@
+package programmers.level1;
+
+import java.util.Arrays;
+
+public class FindDecimal {
+    public static void main(String[] args) {
+        int n = 10;
+        System.out.println(findDecimal(n));
+    }
+    public static int findDecimal(int n) {
+        int count = 0;
+        int[] num = new int[n+1];
+
+        for(int i=2; i<=n; i++) {
+            num[i] = i;
+        }
+        System.out.println(Arrays.toString(num));
+
+        for(int i=2; i<=n; i++) {
+            if(num[i] == 0) continue;
+            for(int j=2*i; j<=n; j+=i) {
+                num[j] = 0;
+            }
+        }
+
+        for(int data:num){
+            if(data != 0){
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    //다른 풀이
+    public static int findDecimal2(int n) {
+        int count = 0;
+        for(int i=2; i<=n; i++) {
+            int j = 2;
+            int cnt = 0;
+            while(j <= (int)Math.sqrt(i)) {
+                if(i % j == 0) {
+                    cnt += 1;
+                    break;
+                }
+                j += 1;
+            }
+            if(cnt == 0) count += 1;
+        }
+        return count;
+    }
+}
