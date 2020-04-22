11635d28e87060b1df2104f0b3dfec31838554fa
youngsun-choi <youngsunchoi42@gmail.com>
2020-04-22 09:23:19 +0900

    Add StringBasic,KimSeoBang

---
+++ src/programmers/level1/KimSeoBang.java
@@ -0,0 +1,26 @@
+package programmers.level1;
+
+import java.util.Arrays;
+
+public class KimSeoBang {
+    public static void main(String[] args) {
+        String[] seoul = {"Jane","Kim"};
+
+        System.out.println(findKim(seoul));
+        System.out.println(findKim1(seoul));
+    }
+
+    public static String findKim(String[] seoul) {
+        int i;
+        for(i=0; i<seoul.length; i++) {
+            if(seoul[i].equals("Kim")) break; //seoul[i].indexOf("Kim") == 0
+        }
+        return "김서방은 "+i+"에 있다";
+    }
+
+    //다른풀이
+    public static String findKim1(String[] seoul) {
+        int x = Arrays.asList(seoul).indexOf("Kim");
+        return "김서방은 "+x+"에 있다";
+    }
+}
---
+++ src/programmers/level1/StringBasic.java
@@ -0,0 +1,36 @@
+package programmers.level1;
+
+public class StringBasic {
+    public static void main(String[] args) {
+        String s = "-123"; //"a234" "1234" "-123"
+        boolean answer = true;
+        int sLength = s.length();
+
+        if(sLength == 4 || sLength == 6) {
+            for(int i=0; i<sLength; i++) {
+                if(s.charAt(i) < 48 || s.charAt(i) > 57) {
+                    answer = false;
+                    break;
+                }
+            }
+        }else {
+            answer = false;
+        }
+
+        System.out.println(answer);
+        System.out.println(solution("11123"));
+    }
+
+    public static boolean solution(String s) {
+        int sLength = s.length();
+
+        //다른풀이
+        if(sLength == 4 || sLength == 6) {
+            return s.matches("(^[0-9]*$)");
+        }
+        return false;
+
+//        return (sLength != 4 && sLength != 6) ||
+//                (s.split("[0-9]").length > 0) ? false : true;
+    }
+}
