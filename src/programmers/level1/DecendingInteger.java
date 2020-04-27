2ae92fef915a840ae6e7558fb7f670f67a7d887a
youngsun-choi <youngsunchoi42@gmail.com>
2020-04-27 15:34:41 +0900

    Add DecendingInteger.java, SquareRoot.java

---
+++ src/programmers/level1/DecendingInteger.java
@@ -0,0 +1,41 @@
+package programmers.level1;
+
+public class DecendingInteger {
+    public static void main(String[] args) {
+        long n = 118372;
+        DecendingInteger decendingInteger = new DecendingInteger();
+        System.out.println(decendingInteger.decending(n));
+        System.out.println(decendingInteger.decending2(n));
+    }
+    public long decending(long n) {
+        String str = "" + n;
+        int strLength = str.length();
+        long[] array = new long[strLength];
+        long tmp;
+        StringBuffer answer = new StringBuffer();
+
+        for(int i=0; i<strLength; i++) {
+            array[i] = n%10;
+            n /= 10;
+        }
+
+        for(int i=0; i<strLength; i++) {
+            for(int j=i+1; j<strLength; j++) {
+                if(array[i] < array[j]) {
+                    tmp = array[j];
+                    array[j] = array[i];
+                    array[i] = tmp;
+                }
+            }
+            answer.append(array[i]);
+        }
+
+        return Long.parseLong(answer.toString());
+    }
+
+    public long decending2(long n) {
+        StringBuffer sb = new StringBuffer();
+        Long.toString(n).chars().sorted().forEach(c -> sb.append(Character.valueOf((char)c)));
+        return Long.parseLong(sb.reverse().toString());
+    }
+}
---
+++ src/programmers/level1/SquareRoot.java
@@ -0,0 +1,18 @@
+package programmers.level1;
+
+public class SquareRoot {
+    public static void main(String[] args) {
+        long n = 13;
+
+        SquareRoot squareRoot = new SquareRoot();
+        System.out.println(squareRoot.root(n));
+        System.out.println(squareRoot.root2(n));
+    }
+    public long root(long n) {
+        return (Math.sqrt(n) == (int)Math.sqrt(n)) ? (long)Math.pow(Math.sqrt(n)+1,2) : -1;
+    }
+    //다른풀이
+    public long root2(long n) {
+        return (Math.pow((int)Math.sqrt(n),2) == n) ? (long) Math.pow(Math.sqrt(n)+1,2) : -1;
+    }
+}
