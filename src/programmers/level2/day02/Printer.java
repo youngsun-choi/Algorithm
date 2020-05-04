547a119f837df62c354a555a17b416df1bd74b06
youngsun-choi <youngsunchoi42@gmail.com>
2020-05-04 13:54:21 +0900

    Add day02

---
+++ src/programmers/level2/day02/Printer.java
@@ -0,0 +1,95 @@
+package programmers.level2.day02;
+
+import java.util.*;
+
+public class Printer {
+    public static void main(String[] args) {
+        int[] priorities = {1, 1, 9, 1, 1, 1}; //{2, 1, 3, 2} {1, 1, 9, 1, 1, 1}
+        int location = 0; //2 0
+
+        Printer printer = new Printer();
+        System.out.println(printer.print(priorities, location));
+        System.out.println(printer.print2(priorities, location));
+        System.out.println(printer.print3(priorities, location));
+    }
+    public int print(int[] priorities, int location) {
+        int answer = 1;
+
+        PriorityQueue pQueue = new PriorityQueue(Collections.reverseOrder());
+
+        for(int data:priorities) {
+            pQueue.add(data);
+        }
+
+        while (!pQueue.isEmpty()) {
+            for(int i=0; i<priorities.length; i++) {
+                if(priorities[i] == (int)pQueue.peek()) {
+                    System.out.println(i+", "+location);
+                    if(i == location) {
+                        return answer;
+                    }
+                    pQueue.poll();
+                    answer++;
+                }
+            }
+        }
+        return  answer;
+    }
+    public int print2(int[] priorities, int location) {
+        int answer = 0;
+        int current;
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for(int data:priorities) {
+            queue.add(data);
+        }
+        Arrays.sort(priorities);
+        int length = priorities.length-1;
+
+        while(!queue.isEmpty()) {
+            current = queue.poll();
+            if(current == priorities[length-answer]) {
+                answer++;
+                location--;
+                if(location<0){
+                    break;
+                }
+            }else {
+                queue.add(current);
+                location--;
+                if(location<0) {
+                    location = queue.size() - 1;
+                }
+            }
+        }
+        return  answer;
+    }
+    public int print3(int[] priorities, int location) { //안됨
+        int pLength = priorities.length;
+        int max = priorities[0] ;
+        int maxIndex = 0;
+        Stack<Integer> stack = new Stack< >();
+
+        for(int i=1; i<pLength; i++) {
+            if(max < priorities[i]) {
+                max = priorities[i];
+                maxIndex = i;
+            }
+        }
+
+        if(max == priorities[location]) {
+            return 1;
+        }else {
+            for(int i=0; i<pLength; i++) {
+                if(max > priorities[i]) {
+                    stack.push(i);
+                    if(i == location) {
+                        break;
+                    }
+                }
+            }
+        }
+        return pLength-maxIndex+stack.size();
+    }
+}
